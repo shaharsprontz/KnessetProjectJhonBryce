32ab8c6e56d8df1f4e60826d7c2b0c6b7506e991
shaharsprontz <sh.sprontz@gmail.com>
2017-01-11 07:59:36 +0200

    some more data on mk

--- js/getMks.js
+++ js/getMks.js
@@ -3,36 +3,13 @@
  */
 
 $(document).ready(function () {
-            $.ajax({
-                method: "GET",
-                url: "http://oknesset.org/api/v2/member/?is_current=true",
-                dataType: 'json',
-                success: function (mkDetails) {
-                    paintMks(mkDetails.objects);
-                }
-            });
-
-            
-            $(".mks li").click(function (e) {
-                e.preventDefault();
-                var mkIds = ($(this).attr('id'));
-                console.log(mkIds);
-
-                var changeLi = 'liChange';
-                ($(".mks li").removeClass(changeLi));
-                $(this).addClass(changeLi);
-
-                $.ajax({
-                    method: "GET",
-                    url: "http://oknesset.org/api/v2/member/" + mkIds,
-                    success: function (data) {
-                        paintMksToDom()
-                    }
-                });
-                });
-
-            $('.mks li').click(function () {
-            $("#mkData").empty();
-            })
+    $.ajax({
+        method: "GET",
+        url: "http://oknesset.org/api/v2/member/?is_current=true",
+        dataType: 'json',
+        success: function (mkDetails) {
+            paintMks(mkDetails.objects);
+        }
+    });
 });
     
--- js/paintMksData.js
+++ js/paintMksData.js
@@ -1,45 +1,45 @@
 /**
  * Created by Shahar on 03/01/2017.
  */
-function paintMksToDom() {
+function paintMksToDom(data) {
 
-    
+    var name = data.name;
+    var role = data.current_role_descriptions;
+    var birth = data.date_of_birth;
+    var party = data.party_name;
+    var start = data.start_date;
+    var email = data.email;
+    var img = data.img_url;
+    var currentPos = data.current_position;
+    var committees = data.committees;
+    var facebook = data.links;
+    var fb = facebook.title;
+    console.log(fb);
 
-        var name = data.name;
-        var role = data.current_role_descriptions;
-        var birth = data.date_of_birth;
-        var party = data.party_name;
-        var start = data.start_date;
-        var email = data.email;
-        var img = data.img_url;
-        var currentPos = data.current_position;
-        var committees = data.committees;
-        var facebook = data.links[0].url;
+    var vaadot = $(committees).each(function (index, value) {
+        var vaadot = value[0];
+        console.log(vaadot);
 
-        //var coom = committees.replace(/\\\//g, "/");
-        var vaadot = $(committees).each(function (index, value) {
-            var vaadot = value[0];
-            console.log(vaadot);
-        })
+    });
 
-        if (data.date_of_birth == null) {
-            birth = "מידע חסר"
-        }
-        if (data.current_role_descriptions == null) {
-            role = "מידע חסר"
-        }
-        if (data.email == null) {
-            email = "מידע חסר"
-        }
-
-        $("#mkData").append($("<img>").attr('src', data.img_url).append(img))
-            .append($("<div>").attr('id', 'mkName').append("שם: " + name))
-            .append($("<div>").attr('id', 'role').append("תפקיד: " + role))
-            .append($("<div>").attr('id', 'birth').append("תאריך לידה: " + birth))
-            .append($("<div>").attr('id', 'party').append("מפלגה: " + party))
-            .append($("<div>").attr('id', 'start').append("תחילת חברות: " + start))
-            .append($("<div>").attr('id', 'position').append("מיקום במפלגה: " + currentPos))
-            .append($("<div>").attr('id', 'committees').append("ועדות: " + vaadot))
-            .append($("<span>").attr('href', facebook).append("פייסבוק: " + facebook + "</br>"))
-            .append($("<a>").attr('href', "mailto:" + email).attr("target", "_top").append(email));
+    if (data.date_of_birth == null) {
+        birth = "מידע חסר"
     }
+    if (data.current_role_descriptions == null) {
+        role = "מידע חסר"
+    }
+    if (data.email == null) {
+        email = "מידע חסר"
+    }
+
+    $("#mkData").append($("<img>").attr('src', data.img_url).append(img))
+        .append($("<div>").attr('id', 'mkName').append("שם: " + name))
+        .append($("<div>").attr('id', 'role').append("תפקיד: " + role))
+        .append($("<div>").attr('id', 'birth').append("תאריך לידה: " + birth))
+        .append($("<div>").attr('id', 'party').append("מפלגה: " + party))
+        .append($("<div>").attr('id', 'start').append("תחילת חברות: " + start))
+        .append($("<div>").attr('id', 'position').append("מיקום במפלגה: " + currentPos))
+        .append($("<div>").attr('id', 'committees').append("ועדות: " + vaadot))
+        .append($("<div>").attr('href', facebook).append("פייסבוק: " + facebook + "</br>"))
+        .append($("<a>").attr('href', "mailto:" + email).attr("target", "_top").append(email));
+}
