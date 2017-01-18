5ac122927160bdb7524b5b90e89cce1a85b25326
shaharsprontz <jawa350640>
2017-01-18 18:20:26 +0200

    moved testIfExist to paintMk

--- js/paintMksData.js
+++ js/paintMksData.js
@@ -17,14 +17,30 @@ function paintMksToDom(data) {
     for (var i = 0; i < facebook.length; i++) {
         if (facebook[i].url.indexOf("facebook") != -1) {
             var fbAddress = facebook[i].url;
+        }else{
+            fbAddress = "Has no FB"
         }
     }
     var comitteeData = committees.map(function(arr) { return (arr[0] || " ").trim(); }).join(', ');
 
-    exist(data);
+    //exist(data);
+    if (data.img_url == null) {
+        img = ("מידע חסר");
+    }
+
+    if (data.date_of_birth == null) {
+        birth = "מידע חסר"
+    }
+    if (data.current_role_descriptions == null) {
+        role = "מידע חסר"
+    }
+    
+    if (data.email =="") {
+        email = ("מידע חסר");
+    }
 
     $("#mkData").append($("<img>").attr('src', data.img_url).append(img))
-        .append($("<div>").attr('id', 'mkName').append("שם: " + name))
+        .append($("<div>").attr('id', 'mkName').append("שם: ").append(name))
         .append($("<div>").attr('id', 'role').append("תפקיד: " + role))
         .append($("<div>").attr('id', 'birth').append("תאריך לידה: " + birth))
         .append($("<div>").attr('id', 'party').append("מפלגה: " + party))
